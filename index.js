694d38834a1016aaacf91655356f5bb348f57cb6
LUCY <wangecikariu.luc@gmail.com>
2023-07-17 21:16:12 +0300

    Array Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(Garfiled) {
+    cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat(Milo) {
+    cats.shift("Milo");
+}
+function appendCat() {
+   return [...cats,"Broom"];
+}
+function prependCat() {
+   return ["Arnold", ...cats];
+}
+function removeLastCat() {
+   return cats.slice(0, -1);
+}
+function removeFirstCat() {
+   return cats.slice(1);
+}
